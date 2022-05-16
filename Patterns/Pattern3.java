f9a3111265840596c3b090441162541db42d4ac0
IshaanSaxena313 <80590066+IshaanSaxena313@users.noreply.github.com>
2022-05-16 21:29:56 +0530

    Add files via upload

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,22 @@
+class Pattern3 {
+    public static void main(String args[]){
+        int i=1,n=5;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }    
+}
+/*
+Pattern:-
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
