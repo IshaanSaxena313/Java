5331d0a4812c06c916c2aa7695fcceb4d4008c9a
IshaanSaxena313 <80590066+IshaanSaxena313@users.noreply.github.com>
2022-05-16 21:20:21 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,8 @@
+//Program to print sum of 2 numbers(without input)
+class Sum{
+	public static void main(String args[]){
+		int a=5,b=6,sum;
+		sum=a+b;
+		System.out.print("The sum of "+a+" and "+b+" is :- "+sum);
+	}
+}
