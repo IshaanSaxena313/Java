99c8ec0cd1b11b79d253c75f159abee81aaeb667
IshaanSaxena313 <80590066+IshaanSaxena313@users.noreply.github.com>
2022-05-17 21:11:23 +0530

    Add files via upload

---
+++ Labelled_Loop.java
@@ -0,0 +1,16 @@
+/*LABELLED LOOP*/
+/*
+outer and inner are labels used to declare name for a loop or a block
+*/
+class Labelled_Loop {
+    public static void main(String args[]){
+        outer:for(int i=1;i<=5;i++){
+            System.out.println();
+            inner:for(int j=1;j<=5;j++){
+                if(j>i)
+                    continue outer;
+                System.out.print(j);
+            }
+        }
+    }    
+}
